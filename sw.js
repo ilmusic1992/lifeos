c411ca6f30cbdf6741b1b70902ac856b4888319a
ilmusic1992 <ilmusic1992@gmail.com>
2026-06-01 17:08:32 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,14 @@
-const CACHE = 'lifeos-v1';
-const ASSETS = ['/', '/index.html'];
-
+// LifeOS Service Worker - no cache, always fresh
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
   self.clients.claim();
 });
-
 self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
+  // Pass through all requests - no caching
+  e.respondWith(fetch(e.request));
 });
